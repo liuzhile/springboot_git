a69f98b9a0653a89f36c8c848d57a7b0ae271880
lzl <377587222@qq.com>
2018-12-17 20:55:35 +0800

    "this is test2"

--- src/main/java/com/baizhi/lzl/SpringbootGitApplication.java
+++ src/main/java/com/baizhi/lzl/SpringbootGitApplication.java
@@ -9,6 +9,7 @@ import tk.mybatis.spring.annotation.MapperScan;
 public class SpringbootGitApplication {
 
     public static void main(String[] args) {
+        System.out.println("this is liu ");
         SpringApplication.run(SpringbootGitApplication.class, args);
     }
 
